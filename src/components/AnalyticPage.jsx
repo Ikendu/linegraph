fe166d2d7f74b3ee6391ca8159cf463bde9cbcee
Chibunud <ndubest56@gmail.com>
2023-12-21 14:45:48 +0100

    analytic page

---
+++ src/components/AnalyticPage.jsx
@@ -0,0 +1,14 @@
+const AnalyticPage = () => {
+  return (
+    <div className='bg-white'>
+      <div className=' p-10 pb-7 text-xl font-semibold'>Analytics</div>
+      <div className='p-10 bg-slate-100'>
+        <div className='grid grid-cols-2 content-evenly w-full '>
+          <button className='bg-white'>20 AUG 2023 - 0 DEC 2023</button>
+          <button className='bg-white'>Operations</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default AnalyticPage
