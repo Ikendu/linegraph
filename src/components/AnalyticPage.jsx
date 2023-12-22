61b87fff2d8f8d80521651bd78754dc2e17a04d7
Chibunud <ndubest56@gmail.com>
2023-12-22 04:08:39 +0100

    analytic summary

--- src/components/AnalyticPage.jsx
+++ src/components/AnalyticPage.jsx
@@ -6,31 +6,31 @@ const AnalyticPage = () => {
       <div className=' p-10 pb-7 text-xl font-semibold'>Analytics</div>
       <div className='p-10 bg-slate-100 m-10'>
         <div className='flex relative justify-between '>
-          <button className='bg-white rounded-xl p-3 px-6 flex gap-3'>
+          <button className='bg-white rounded-xl p-3 px-8 flex gap-5 font-bold'>
             20 AUG 2023 - 0 DEC 2023 <DownIcon />
           </button>
-          <button className='bg-white rounded-xl p-3 px-6 flex gap-28'>
+          <button className='bg-white rounded-xl p-3 px-6 flex gap-32 font-bold'>
             Operations <DownIcon />
           </button>
         </div>
         <div className='flex justify-between mt-8'>
           <div className='flex gap-10 '>
-            <span>
+            <span className='bg-white p-10 rounded-lg'>
               <p>All conversations</p>
-              <h3></h3>
+              <h3 className=' text-center font-bold text-3xl'>69</h3>
             </span>
-            <span>
-              <p>All conversations</p>
-              <h3></h3>
+            <span className='bg-white p-10 rounded-lg'>
+              <p>Handled conversations</p>
+              <h3 className=' text-center font-bold text-3xl'>20</h3>
             </span>
-            <span>
-              <p>All conversations</p>
-              <h3></h3>
+            <span className='bg-white p-10 rounded-lg'>
+              <p>Missed conversations</p>
+              <h3 className=' text-center font-bold text-3xl'>15</h3>
             </span>
           </div>
-          <div>
-            <p>All conversations</p>
-            <h3></h3>
+          <div className='bg-white p-10 rounded-lg'>
+            <p>Conversations Taken over by bot</p>
+            <h3 className=' text-center font-bold text-3xl'>0</h3>
           </div>
         </div>
       </div>
