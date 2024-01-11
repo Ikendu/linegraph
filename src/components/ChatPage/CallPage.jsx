3e52f4f55f97ac77513d87b519e9bb0f504cab52
Chibunud <ndubest56@gmail.com>
2024-01-11 14:40:34 +0100

    audio call function

--- src/components/ChatPage/CallPage.jsx
+++ src/components/ChatPage/CallPage.jsx
@@ -1,11 +1,18 @@
 //import Mic from './Icons/Mic.svg'
+import DotVert from './Icons/DotVert.svg'
+import attarch from './Icons/attarch.svg'
+import call from './Icons/call.svg'
+import camSlach from './Icons/camSlach.svg'
+import micblack from './Icons/micblack.svg'
 import microphone from './Icons/microphone.svg'
 import minimise from './Icons/minimize.svg'
+import smile2 from './Icons/smile2.svg'
+import voice from './Icons/voice.svg'
 
 const CallPage = () => {
   return (
-    <div className="bg-[#1b1349] w-4/5 pb-[0.5px]">
-      <div className="text-gray-100  p-5 m-10 text-sm">
+    <div className="bg-[#1b1349] w-4/5 pb-[0.5px] max-h-[100vh]">
+      <div className="text-gray-100  p-5 m-10 text-sm ">
         <div className="flex justify-between">
           <div className="flex gap-5">
             <p className="px-6 py-2 bg-[#0f0f3a] rounded-lg ">Evans David</p>
@@ -22,13 +29,16 @@ const CallPage = () => {
           </div>
         </div>
       </div>
-      <div className="border border-[#429AFF]  border-1 p-3 bg-[#0f0f3a] m-auto justify-center rounded-full flex w-48 ">
-        <div className="p-14 px-16  bg-[#b3adad] rounded-full flex text-6xl font-semibold ">
+      <div className="border border-[#429AFF] relative  border-1 p-3 bg-[#0f0f3a] m-auto justify-center rounded-full flex w-52 ">
+        <div className="p-14 px-[70px]  bg-[#706060] rounded-full flex text-6xl font-semibold text-[#a5988d]">
           S
         </div>
+        <div className="absolute bottom-10 right-14 p-2 bg-red-600 rounded-full w-8">
+          <img src={voice} />
+        </div>
       </div>
-      <div className="m-5 mt-32">
-        <div className=" bg-[#534158] w-52">
+      <div className="m-5 mt-28 flex justify-between items-center">
+        <div className=" bg-[#534158] w-60">
           <div className="pt-10 px-10 text-center text-[58px] font-semibold text-gray-500">
             <p>F</p>
           </div>
@@ -40,13 +50,25 @@ const CallPage = () => {
             </div>
           </div>
         </div>
-        <div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="flex gap-2">
+          <div>
+            <img src={micblack} />
+          </div>
+          <div>
+            <img src={camSlach} />
+          </div>
+          <div className="p-2 rounded-full bg-slate-300">
+            <img src={smile2} />
+          </div>
+          <div className="p-2 px-[15px] rounded-full bg-slate-300">
+            <img src={attarch} />
+          </div>
+          <div>
+            <img src={DotVert} />
+          </div>
+          <div>
+            <img src={call} />
+          </div>
         </div>
       </div>
     </div>
